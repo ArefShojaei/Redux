1721b0d1921e4c78d8b64ed46dba92463e026f61
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-03 15:33:53 +0330

    ADD: add Store interface & class

---
+++ src/Store.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Redux;
+
+
+/**
+ * @package
+ */
+use Redux\Contracts\Interfaces\Store as StoreContract;
+
+
+
+/**
+ * Store state
+ * @class
+ */
+class Store implements StoreContract {
+    /**
+     * Reducer
+     * @prop
+     * @private
+     * @type callable
+     */
+    private $reudcer;
+    
+    /**
+     * Middlewares
+     * @prop
+     * @private
+     * @type array
+     */
+    private array $middlewares;
+
+
+    /**
+     * Constructor
+     * @param callable $reducer
+     * @param array $middlewares
+     */
+    public function __construct(callable $reudcer, array $middlewares)
+    {
+        $this->reudcer = $reudcer;
+        $this->middlewares = $middlewares;
+    }
+}
