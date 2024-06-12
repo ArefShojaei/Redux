44bb042338abeff040b6ac11a39f0e066a759397
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-12 15:54:36 +0330

    ADD: add to create action test

--- tests/Unit/ActionTest.php
+++ tests/Unit/ActionTest.php
@@ -10,4 +10,24 @@ use Redux\Action;
 /**
  * Action Test
  */
-class ActionTest extends TestCase {}
+class ActionTest extends TestCase {
+    /**
+     * @test
+     */
+    public function createAction() {
+        # Arrange
+        $incrementActionType = "@INIT";
+        $expected = [
+            "type" => $incrementActionType,
+            "payload" => null
+        ];
+        
+        # Act
+        $incrementAction = new Action($incrementActionType);
+        $actual = $incrementAction();
+
+
+        # Assert
+        $this->assertEquals($expected, $actual);
+    }
+}
