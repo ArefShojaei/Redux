ba049a79e1d625480ceebeed4893946f32ea4661
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-12 16:35:34 +0330

    ADD: add to create Reducer test

--- tests/Unit/ReducerTest.php
+++ tests/Unit/ReducerTest.php
@@ -11,5 +11,22 @@ use Redux\Reducer;
  * Reducer Test
  */
 final class ReducerTest extends TestCase {
+    private const INIT_STATE = 0;
+    private const DEFAULT_REDUCERS = [];
 
+    /**
+     * @test
+     */
+    public function createReducer() {
+        # Arrange
+        
+        # Act
+        $reducer = new Reducer(self::INIT_STATE, self::DEFAULT_REDUCERS);
+
+        $actual = $reducer->getReducer();
+        
+
+        # Assert
+        $this->assertIsCallable($actual);
+    }
 }
