36c8ed10680a9e826d5f7e2a74208610336d0d3d
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-12 19:40:11 +0330

    ADD: add to create Store test

--- tests/Integration/StoreTest.php
+++ tests/Integration/StoreTest.php
@@ -4,6 +4,8 @@
  * @package
  */
 use PHPUnit\Framework\TestCase;
+use Redux\Action;
+use Redux\Reducer;
 use Redux\Store;
 
 
@@ -11,4 +13,39 @@ use Redux\Store;
 /**
  * Store Test
  */
-final class StoreTest extends TestCase {}
+final class StoreTest extends TestCase {
+    private $reducer;
+    private $action;
+
+
+    protected function setup(): void {
+        # Action
+        $this->action = new Action("INCREMENT");
+        
+
+        # Reducer
+        $initState = 0;
+        $incrementAction = $this->action;
+
+        $reducers = [
+            $incrementAction()['type'] => fn($state, $aciton) => $state + 1
+        ];
+
+        $this->reducer = new Reducer($initState, $reducers);
+    }
+
+
+    /**
+     * @test
+     */
+    public function createStore() {
+        # Arrange
+        $reducer = $this->reducer->getReducer();
+        
+        # Act
+        $store = new Store($reducer);
+    
+        # Assert
+        $this->assertIsObject($store);
+    }
+}
