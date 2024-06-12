b527377cedaaf8dee40b5e409433dc6478aa461c
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-12 19:06:48 +0330

    ADD: add to create Reducer test

--- tests/Unit/ReducerTest.php
+++ tests/Unit/ReducerTest.php
@@ -11,4 +11,35 @@ use Redux\Contracts\Interfaces\Reducer as ReducerContract;
 /**
  * Reducer Test
  */
-final class ReducerTest extends TestCase {}
+final class ReducerTest extends TestCase {
+    private $initState;
+    private $reducers;
+    
+
+
+    protected function setup(): void {
+        $this->initState = 0;
+
+        $this->reducers = [
+            "INCREMENT" => fn($state, $action) => $state + 1,
+            "DECREMENT" => fn($state, $action) => $state - 1,
+            "RESET" => fn($state, $action) => $state - $state
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function createReducer() {
+        # Arrange
+        $reducer = new Reducer($this->initState, $this->reducers);
+
+        # Act
+        $actual = $reducer->getReducer();
+
+        # Assert
+        $this->assertIsCallable($actual);
+
+        return $reducer;
+    }
+}
